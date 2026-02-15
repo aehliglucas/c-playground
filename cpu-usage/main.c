15fa2ad18ecca83c892dd880fc5c0558375e18a7
aehliglucas <lucas@aehlig.com>
2026-02-15 14:31:28 +0100

    add utilization calculation and output

--- cpu-usage/main.c
+++ cpu-usage/main.c
@@ -1,20 +1,35 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+// Logic to catch SIGINT to close the file handler
+volatile sig_atomic_t g_running = true;
+void intHandler(int signum)
+{
+    g_running = false;
+}
 
 struct cpuUtilization
 {
-    unsigned long long user, nice, system, idle, iowait, irq, softirq;
+    long long user, nice, system, idle, iowait, irq, softirq;
+};
+
+struct cpuUtilization previousUtilization = {
+    0, 0, 0, 0, 0, 0, 0
 };
 
 struct cpuUtilization getCurrentUtilization(FILE* file)
 {
-    char* buffer[64];
-    if (fgets(*buffer, 64, file) == NULL) {
+    char buffer[256];
+    if (fgets(buffer, 256, file) == NULL) {
         fprintf(stderr, "Can't read CPU data from buffer");
+        fclose(file);
         exit(1);
     }
-    strtok(*buffer, " ");
+    strtok(buffer, " ");
 
     long long cpuData[64];
     int count = 0;
@@ -32,20 +47,56 @@ struct cpuUtilization getCurrentUtilization(FILE* file)
         cpuData[3],
         cpuData[4],
         cpuData[5],
+        cpuData[6],
     };
 
     return currentUtilization;
 }
 
+struct utilizationTimeDelta
+{
+    long long deltaWorkTime;
+    long long deltaTotalTime;
+};
+
+struct utilizationTimeDelta calculateTimeDelta(struct cpuUtilization currentUtilization)
+{
+    struct utilizationTimeDelta result;
+    long long oldWorkTime = previousUtilization.user + previousUtilization.nice + previousUtilization.system + previousUtilization.irq + previousUtilization.softirq;
+    long long oldTotalTime = oldWorkTime + previousUtilization.idle + previousUtilization.iowait;
+    long long newWorkTime = currentUtilization.user + currentUtilization.nice + currentUtilization.system + currentUtilization.irq + currentUtilization.softirq;
+    long long newTotalTime = newWorkTime + currentUtilization.idle + currentUtilization.iowait;
+
+    result.deltaWorkTime = llabs(newWorkTime - oldWorkTime);
+    result.deltaTotalTime = llabs(newTotalTime - oldTotalTime);
+
+    return result;
+};
+
 int main(void) {
     FILE* file = fopen("/proc/stat", "r");
+    // Disable C-internal file buffering for /proc/stat
+    setvbuf(file, NULL, _IONBF, 0);
 
     if (file == NULL) {
         fprintf(stderr, "Can't open /proc/stat");
         return 1;
     }
 
-    struct cpuUtilization currentUtilization = getCurrentUtilization(file);
+    while (g_running)
+    {
+        rewind(file);
+        struct cpuUtilization currentUtilization = getCurrentUtilization(file);
+        struct utilizationTimeDelta timeDelta = calculateTimeDelta(currentUtilization);
+        if (timeDelta.deltaWorkTime == 0 || timeDelta.deltaTotalTime == 0)
+        {
+            continue;
+        }
+        printf("\r%3lld   ", 100 * timeDelta.deltaWorkTime / timeDelta.deltaTotalTime);
+        fflush(stdout);
+        previousUtilization = currentUtilization;
+        sleep(1);
+    }
 
     fclose(file);
     return 0;
